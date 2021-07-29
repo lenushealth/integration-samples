ba2980c0925898deab1ff360f23896550f94fed8
philo <philip.oyston@lenushealth.com>
2021-07-29 19:03:43 +0100

    Add bicep deploy templates to each sample, using common deploy modules

---
+++ samples/dotnet/clinorg/src/Pages/Error.cshtml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Lenus.Samples.ClinicianOrg.Pages
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
+    public class ErrorModel : PageModel
+    {
+        public string RequestId { get; set; } = string.Empty;
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        [BindProperty(SupportsGet = true)]
+        public string Message { get; set; }
+
+        private readonly ILogger<ErrorModel> _logger;
+
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
