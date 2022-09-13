cfb6b38f075155613f810c07968e8457e5674082
Ashleigh Adams <Ashleigh.Adams@lenushealth.com>
2022-09-13 15:28:20 +0100

    Fixing whiteplace issues

--- samples/dotnet/clientcreds/Lenus.Samples.ClientCredentialsFlow/src/Configuration/LenusClientOptions.cs
+++ samples/dotnet/clientcreds/Lenus.Samples.ClientCredentialsFlow/src/Configuration/LenusClientOptions.cs
@@ -1,7 +1,7 @@
 ﻿namespace Lenus.Samples.ClientCredentialsFlow.Configuration
 {
-	public class LenusClientOptions
-	{
+    public class LenusClientOptions
+    {
         public string TokenUrl { get; set; } = string.Empty;
         public string ClientId { get; set; } = string.Empty;
         public string ClientSecret { get; set; } = string.Empty;
