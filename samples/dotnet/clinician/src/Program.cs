6c8ce617d608d98666f2ca7c7e6b00e921546cfb
andrew.hendry <andrew.hendry@lenushealth.com>
2023-02-20 14:59:19 +0000

    Upping clinician to .NET 7.

--- samples/dotnet/clinician/src/Program.cs
+++ samples/dotnet/clinician/src/Program.cs
@@ -1,26 +1,68 @@
-﻿using Microsoft.AspNetCore;
+﻿using Clinician;
+using Clinician.Controllers;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Diagnostics;
+using System.Globalization;
+using System.Text.Json;
 
-namespace Clinician
+
+Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+Activity.ForceDefaultIdFormat = true;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(k => k.AddServerHeader = false);
+
+builder.Services.AddLocalization();
+builder.Services.AddMvc().AddJsonOptions(o =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Activity.DefaultIdFormat = ActivityIdFormat.W3C;
-            Activity.ForceDefaultIdFormat = true;
+    o.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    o.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+});
 
-            CreateHostBuilder(args).Build().Run();
-        }
+builder.Services.Configure<RouteOptions>(r =>
+{
+    r.LowercaseUrls = true;
+    r.ConstraintMap.Add("sampletype", typeof(SampleDataTypeRouteConstraint));
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.ConfigureKestrel(o => o.AddServerHeader = false);
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddLenusAuthentication(builder.Configuration);
+builder.Services.AddLenusAuthorisation();
+builder.Services.AddLenusHealthClient(builder.Configuration);
+builder.Services.AddAgencyServices(builder.Configuration);
+
+builder.Services.AddMemoryCache();
+builder.Services.AddApplicationInsightsTelemetry();
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseRequestLocalization();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error");
 }
+
+app.UseStaticFiles();
+
+var gbCulture = CultureInfo.GetCultureInfo("en-GB");
+app.UseRequestLocalization(
+    new RequestLocalizationOptions()
+    {
+        DefaultRequestCulture = new RequestCulture(gbCulture),
+        SupportedCultures = new[] { gbCulture },
+        SupportedUICultures = new[] { gbCulture }
+    });
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapDefaultControllerRoute();
+
+app.Run();
